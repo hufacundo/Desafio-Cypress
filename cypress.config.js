85f17312d230dddf373ff5a8e0943df92a634e11
Hugo Facundo <hufacundo@hotmail.com>
2022-12-15 20:30:56 -0300

    Iserindo projeto

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
   e2e: {
 
-    baseUrl:'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', 
+    baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',
     viewportWidth: 1440, viewportWidth: 900,
+    //conectando projeto ao dashboard do cypress:
+    projectId: "is66ix",
+
+
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    "chromeWebSecurity":false,
   },
 });
